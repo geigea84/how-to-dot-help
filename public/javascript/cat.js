66e63917a3191743f714d454d9b84b0c995c0952
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-12-18 12:08:01 -0600

    basic user forms linked to local storage

--- public/javascript/cat.js
+++ public/javascript/cat.js
@@ -1,4 +1,47 @@
+//text for times
+var firstName = localStorage.getItem("firstName")
+var lastName = localStorage.getItem("lastName")
+var email = localStorage.getItem("email")
+var phone = localStorage.getItem("phone")
+var info = localStorage.getItem("info")
+var contact = localStorage.getItem("contact")
 
+//on load: 
+$(document).ready(function () {
+//cound have writen the following section as :
+//$( "#9am #text").html(localStorage.getItem("9am"));
+      $("#first-name").html( firstName );
+      $("#last-name").html( lastName );
+      $("#email").html( email );
+      $("#phone").html( phone );
+      $("#info").html( info );
+      $("#contact").html( contact );
+})
+
+
+//save it! 
+$("#save").on("click", function() {
+    console.log("click happend")
+    //collect text
+    FN = $("#first-name").val()
+    LN = $("#last-name").val()
+    E = $("#email").val()
+    P = $("#phone").val()
+    I = $("#info").val()
+    C = $("#contact").val()
+        //save text
+  var saveTask = function () {
+    localStorage.setItem("firstName", FN)
+    localStorage.setItem("lastName", LN) 
+    localStorage.setItem("email", E) 
+    localStorage.setItem("phone", P) 
+    localStorage.setItem("info", I) 
+    localStorage.setItem("contact", C)    
+  }
+    saveTask()
+  });
+
+//Fade and Show Effects------------------------------------------------
   $(document).ready(function () {
     $("#nav").show(2000);
 });
