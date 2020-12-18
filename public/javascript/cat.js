5f6567908d4c7e11d64e667fc6d9025ffab99c3b
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-12-17 23:00:06 -0600

    added in j-query and starting to update forms to me usable

---
+++ public/javascript/cat.js
@@ -0,0 +1,25 @@
+
+  $(document).ready(function () {
+    $("#nav").show(2000);
+});
+
+$(document).ready(function () {
+    $("#main-line").fadeIn(3000);
+});
+//HIDE V FORM----------------------------------------------------------
+function hideVForm() {
+    $("#form-v").slideToggle(1000);
+  
+    var Button = document.getElementById("hide-vinfo");
+    var ButtonText = Button.innerHTML
+    console.log(ButtonText)
+    if (ButtonText=="Open My Info") {
+      Button.innerHTML = "Close Info"
+    }
+    else {Button.innerHTML = "Open My Info"
+  }
+  
+  }
+  var hideVolunteerForm = document.getElementById("hide-vinfo")
+  hideVolunteerForm.addEventListener("click", hideVForm);
+  
