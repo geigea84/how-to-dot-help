d14918859ca3d7a6c1f7966f6aadc50c719906d4
uberbrent <bmjohns1337@gmail.com>
2020-12-21 20:33:10 -0600

    commiting for merge

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 const path = require('path');
 const session = require('express-session');
-//const exphbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -19,7 +19,8 @@ const sess = {
     })
 };
 
-//app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
