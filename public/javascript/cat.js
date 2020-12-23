d7b35b9291bda9daa562820aa0f672a7fa88e2aa
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-12-23 17:11:24 -0600

    user page now includes nfp sign ups

--- public/javascript/cat.js
+++ public/javascript/cat.js
@@ -10,20 +10,20 @@ $(document).ready(function () {
     $("#main-line").fadeIn(3000);
 });
 //HIDE V FORM----------------------------------------------------------
-// function hideVForm() {
-//     $("#form-v").slideToggle(1000);
+function hideVForm() {
+    $("#form-v").slideToggle(1000);
   
-//     var Button = document.getElementById("hide-vinfo");
-//     var ButtonText = Button.innerHTML
-//     console.log(ButtonText)
-//     if (ButtonText=="Open My Info") {
-//       Button.innerHTML = "Close Info"
-//     }
-//     else {Button.innerHTML = "Open My Info"
-//   }
-//   }
-//   var hideVolunteerForm = document.getElementById("hide-vinfo")
-//   hideVolunteerForm.addEventListener("click", hideVForm);
+    var Button = document.getElementById("hide-vinfo");
+    var ButtonText = Button.innerHTML
+    console.log(ButtonText)
+    if (ButtonText=="Open My Info") {
+      Button.innerHTML = "Close Info"
+    }
+    else {Button.innerHTML = "Open My Info"
+  }
+  }
+  var hideVolunteerForm = document.getElementById("hide-vinfo")
+  hideVolunteerForm.addEventListener("click", hideVForm);
   
   //HIDE NUTRAL----------------------------------------------------------
 // function hideInfoToggle() {
