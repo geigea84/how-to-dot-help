c94992b1a859e6ccfaafde6a4428daba40a5994a
Cat <CGraham@CatherinesMBP2.attlocal.net>
2020-12-22 19:48:28 -0600

    get volunteers rout

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,4 @@
-const { Sequelize } = require('sequelize/types');
+
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 
@@ -18,10 +18,28 @@ router.get('/signup', (req, res) => {
     res.render('join');
 });
 
-//////////////////////////PERSONAL PAGE/////////////////////////////////////
+//------------------------------------------------------------------------//
+//////////////////////////USER GET INFO/////////////////////////////////////
+//------------------------------------------------------------------------//
+
+let formatPhoneNumber = (P) => {
+    //Filter only numbers from the input
+    let cleaned = ('' + P).replace(/\D/g, '');
+    
+    //Check if the input is of correct length
+    let match = cleaned.match(/^(\d{3})(\d{3})(\d{4})$/);
+  
+    if (match) {
+      return '(' + match[1] + ') ' + match[2] + '-' + match[3]
+    }; 
+    return " "
+  };
+
 
 // personal page //
 router.get('/volunteer/:id', (req, res) => {
+
+    console.log('HOMEROUTES')
     Volunteer.findOne({
       where: {
         id: req.params.id
@@ -44,7 +62,11 @@ router.get('/volunteer/:id', (req, res) => {
         }
 
         const volunteer = dbPostData.get({ plain: true });
-  
+       // console.log(volunteer.phone_number)
+        var num = formatPhoneNumber(volunteer.phone_number)
+        console.log(num)
+        volunteer.phone_number = num;
+        volunteer.id =  req.params.id;
         res.render('volunteers', {
           volunteer,
           loggedIn: req.session.loggedIn
@@ -56,6 +78,48 @@ router.get('/volunteer/:id', (req, res) => {
       });
   });
   
+
+//------------------------------------------------------------------------//
+//////////////////////////USER PUT INFO/////////////////////////////////////
+//------------------------------------------------------------------------//
+
+// personal page //
+router.put('/volunteer/:id', (req, res) => {
+
+    console.log('put', req.body, req.params)
+    Volunteer.update(
+        {where: {
+            id: req.params.id
+          }},
+     {  first_name: req.body.userinfo.first_name,
+        last_name: req.body.userinfo.last_name,
+        email: req.body.userinfo.email,
+        phone_number: req.body.userinfo.phone_number,
+        bio: req.body.userinfo.bio,
+        state: req.body.userinfo.state,
+        city: req.body.userinfo.city
+    }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No user found with this id' });
+          return;
+        }
+        console.log(dbPostData)
+
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+
+
+
+
+
+
 
 
   ////////////////////////////////////////////////////////
@@ -82,7 +146,7 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 });
 
-router.get('/volunteer', (req, res) => {
+router.get('/volunteer/1', (req, res) => {
     res.render('/volunteers');
 });
 
