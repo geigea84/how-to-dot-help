50ac03ee2de3f0770c0c9235af9255216167c0a5
Cat <CGraham@CatherinesMBP2.attlocal.net>
2021-01-01 23:11:14 -0600

    added id to btn on nfp page in prep for making nfpuserconnection

--- public/javascript/nonRoutes.js
+++ public/javascript/nonRoutes.js
@@ -28,12 +28,14 @@ function hideVForm() {
   }
   }
 //carried over code fro merge
-  var hideVolunteerForm = document.getElementById("hide-vinfo")
+  const hideVolunteerForm = document.getElementById("hide-vinfo")
   hideVolunteerForm.addEventListener("click", hideVForm);
 //end caryover
 
-  $('.reach-out').click(function(){
-    console.log("clicked btn")
-    $(this).fadeOut(3000);
-  })
+//  const reachOutResponse = function() {
+//     console.log("clicked btn")
+//     $(this).fadeOut(3000);
+//   }
 
+  const reachOutBtnClicked = document.getElementsByClassName("reach-out")
+  reachOutBtnClicked.addEventListener("click",console.log("btn clicked"))
