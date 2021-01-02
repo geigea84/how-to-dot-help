afdaf32e27f7ee96ec061e8788c7c9b9a0367441
Cat <CGraham@CatherinesMBP2.attlocal.net>
2021-01-02 14:49:00 -0600

    prep for murge

--- public/javascript/partner.js
+++ public/javascript/partner.js
@@ -1,9 +1,33 @@
+async function reachOut(i) {
+    event.preventDefault();
+    console.log(i)
+  
+    if (i) {
+      const response = await fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          nfp_id,
+          user_id
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/user/:id');
+      } else {
+        alert(response.statusText);
+      }
+    }
+  }
 
 
-console.log("IM CONNECTED!!!")
+
+
  const reachOutResponse = function() {
     console.log("clicked btn")
+    var nfp_id = $(this).attr("id");
     $(this).fadeOut(3000);
+      reachOut(nfp_id)
   }
 
 $(document).on("click", ".reach-out", reachOutResponse)
