b450a2c1a50104c65bd7ee116dea9cd0060dbfe8
Cat <CGraham@CatherinesMBP2.attlocal.net>
2021-01-02 11:29:31 -0600

    buttons on partials now work! see partners.js for example

--- public/javascript/partner.js
+++ public/javascript/partner.js
@@ -1,23 +1,9 @@
-// var source = document.innerHTML; 
-var template = Handlebars.compile(source);
-document.body.innerHTML = template();
-// var partnerNFP = document.getElementsByClassName("joinNFPnow");
 
-console.log(partnerNFP)
+
+console.log("IM CONNECTED!!!")
  const reachOutResponse = function() {
     console.log("clicked btn")
     $(this).fadeOut(3000);
   }
 
-const reachOutBtnClicked = document.getElementsByClassName("reach-out")
-reachOutBtnClicked.addEventListener("click",console.log("btn clicked"))
-
-
-
-// // // Create a new task
-// // partnerNFP.addEventListener("click", reachOutResponse);
-// document.getElementById("joinNFPnow").addEventListener("click", myFunction);
-
-// function myFunction() {
-//   document.getElementById("joinNFPnow").innerHTML = "YOU CLICKED ME!";
-// }
+$(document).on("click", ".reach-out", reachOutResponse)
