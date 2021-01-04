466d0aebd64eed43b5a41aeb1d367a85b92197fa
geigea84 <geigea84@gmail.com>
2021-01-03 20:01:16 -0600

    updated database connections to connect to heroku

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ else {
         process.env.DB_USER,
         process.env.DB_PW,
         {
-            host: "localhost",
+            host: "z3iruaadbwo0iyfp.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
             dialect: "mysql",
             port: 3306
         }
